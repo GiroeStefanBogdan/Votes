7386edfc63a63b948d4a8095c4b88a7c032d547e
GiroeStefanBogdan <Giroe_1999@protonmail.com>
2021-11-06 16:33:31 +0200

    Adding entitys Users, Feature, Comment and Vote and connect to the database

---
+++ src/main/java/com/votes/entity/VoteClass.java
@@ -0,0 +1,32 @@
+package com.votes.entity;
+
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+
+@Entity
+public class VoteClass {
+
+	
+	private VoteId pk; //VoteId will be a composite key with user_id and feature_id
+	private Boolean upvote;
+
+	public Boolean getUpvote() {
+		return upvote;
+	}
+
+	public void setUpvote(Boolean upvote) {
+		this.upvote = upvote;
+	}
+
+	@EmbeddedId
+	public VoteId getPk() {
+		return pk;
+	}
+
+	public void setPk(VoteId pk) {
+		this.pk = pk;
+	}
+	
+}
