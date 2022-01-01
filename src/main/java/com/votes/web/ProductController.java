99b51843a787499cb72237176b34ac6119005014
GiroeStefanBogdan <Giroe_1999@protonmail.com>
2022-01-01 18:59:22 +0200

    Retrieving Product Data from DB

--- src/main/java/com/votes/web/ProductController.java
+++ src/main/java/com/votes/web/ProductController.java
@@ -1,6 +1,12 @@
 package com.votes.web;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.votes.entity.Product;
 import com.votes.entity.User;
 import com.votes.repositories.ProductRepository;
+
+import javassist.NotFoundException;
 
 @Controller
 public class ProductController {
@@ -24,7 +32,17 @@ public class ProductController {
 	}
 	
 	@GetMapping("/products/{productId}")
-	public String getProduct(@PathVariable Long productId) {
+	public String getProduct(@PathVariable Long productId, ModelMap model, HttpServletResponse response) throws IOException {
+	 Optional<Product> productOpt = productRepo.findById(productId);
+	 
+	 if(productOpt.isPresent()) {
+		 Product product = productOpt.get();
+		 model.put("product", product);
+	 }else {
+		 response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " was not found");
+		 return "product";
+	 }
+	 
 		return "product";
 	}
 	
